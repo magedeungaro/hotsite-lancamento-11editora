1321deae6522cc3d64e236e8eba2595ce014560f
magedeungaro <magedbgt@gmail.com>
2023-09-09 01:37:11 -0300

    botao desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,14 @@ export default function Home() {
         <Image src="/hero.png"
               alt="Banner com desconto de 11% no livro Homem Extraordinário, além de brindes e recompensas"
               width={1920} height={1080} className='min-w-full hero' />
+
+        <div className="grid grid-cols-3 grid-rows-9 gap-1 min-w-full absolute top-3/4">
+          <div className="opacity-0"></div>
+          <div className="opacity-0"></div>
+          <div className='flex align-center justify-center'>
+            <button className='rounded-full primary-color-bg hero-btn'>Comprar</button>
+          </div>
+        </div>
       </div>
     </main>
   )
