17093250380f671bea9269916da09af9b7e08904
magedeungaro <magedbgt@gmail.com>
2023-09-09 00:37:06 -0300

    banner responsivo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen min-w-full flex-col items-center justify-between">
-      <Image src="/hero.png"
-             alt="Banner com desconto de 11% no livro Homem Extraordinário, além de brindes e recompensas"
-             layout='responsive' width={1920} height={1080} />
+    <main className="min-h-screen">
+      <div className="image-container relative">
+        <Image src="/hero.png"
+              alt="Banner com desconto de 11% no livro Homem Extraordinário, além de brindes e recompensas"
+              width={1920} height={1080} className='min-w-full hero' />
+      </div>
     </main>
   )
 }
