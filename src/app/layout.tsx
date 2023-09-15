d24373c56471833e6983bc4081148d918c888055
magedeungaro <magedbgt@gmail.com>
2023-09-15 19:20:37 -0300

    og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
         <meta property="og:description" content="Lançamento 11 Editora - Homem Extraordinário com 11% de desconto, brindes e recompensas"/>
         <meta property="og:type" content="book"/>
         <meta property="og:image" content="https://homem-extraordinario.11editora.com.br/hero-og.jpg"/>
+        <meta content="image/jpeg" property="og:image:type"/>
+        <meta content="1061" property="og:image:width"/>
+        <meta content="597" property="og:image:height"/>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
