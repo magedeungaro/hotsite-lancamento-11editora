784d0c5b6e0092ef24568e79df2326f10162d8c6
magedeungaro <magedbgt@gmail.com>
2023-09-15 18:31:44 -0300

    og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,15 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Homem Extraordinário - Lançamento 11 Editora',
   description: 'Lançamento 11 Editora - Homem Extraordinário com 11% de desconto, brindes e recompensas',
-}
+  openGraph: {
+    title: 'Homem Extraordinario',
+    siteName: '11 Editora',
+    url: 'https://homem-extraordinario.11editora.com.br/',
+    description: 'Lançamento 11 Editora - Homem Extraordinário com 11% de desconto, brindes e recompensas',
+    type: 'book',
+    images: 'https://homem-extraordinario.11editora.com.br/hero.png',
+  },
+};
 
 export default function RootLayout({
   children,
