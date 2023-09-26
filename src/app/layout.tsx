41cf043c5706dc7536c898bbc283293c59021ab7
magedeungaro <magedbgt@gmail.com>
2023-09-26 11:18:08 -0300

    fix typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="shortcut icon" href="/favicon.png" />
-        <meta property="og:title" content="Homem Extraordinario"/>
+        <meta property="og:title" content="Homem Extraordinário"/>
         <meta property="og:site_name" content="11 Editora"/>
         <meta property="og:url" content="https://homem-extraordinario.11editora.com.br/"/>
         <meta property="og:description" content="Lançamento 11 Editora - Homem Extraordinário com 11% de desconto, brindes e recompensas"/>
